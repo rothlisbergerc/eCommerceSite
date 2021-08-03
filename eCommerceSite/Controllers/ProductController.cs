0043ecc91f6ed8d2e628dcefab4a730329246494
Cheryl Rothlisberger <77075506+rothlisbergerc@users.noreply.github.com>
2021-08-03 11:50:06 -0700

    Issue#6 product listing (#8)
    * Created product listing page
    
    * Styled product catalog
    
    * Refactored catalog to use model properties as column headers.

---
+++ eCommerceSite/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Controllers
+{
+    public class ProductController : Controller
+    {
+        // Construction injection
+        private readonly ProductContext _context;
+
+        public ProductController(ProductContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Displays a view that lists all products
+        /// </summary>
+        public IActionResult Index()
+        {
+            // Get all products from the database
+            List<Product> products = _context.Products.ToList();
+
+            // Send list of products to view to be displayed
+            return View(products);
+        }
+    }
+}
