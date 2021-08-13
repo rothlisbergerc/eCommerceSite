7e9abe883a32c5466afd363108fc145e749f84bd
rothlisbergerc <rothlisbergercheryl@gmail.com>
2021-08-13 09:32:36 -0700

    Refactor DB code into separate class

--- eCommerceSite/Controllers/ProductController.cs
+++ eCommerceSite/Controllers/ProductController.cs
@@ -29,22 +29,12 @@ namespace eCommerceSite.Controllers
             const int PageSize = 3;
             ViewData["CurrentPage"] = pageNum;
 
-            int numProducts = await (from p in _context.Products
-                               select p).CountAsync(); // returns one product from the database
+            int numProducts = await ProductDB.GetTotalProductsAsync(_context);
             int totalPages = (int)Math.Ceiling((double)numProducts / PageSize);
-
             ViewData["MaxPage"] = totalPages;
 
-
-            // Get all products from the database
-            // List<Product> products = _context.Products.ToList();
-            List < Product > products =
-                await (from p in _context.Products
-                       orderby p.Title ascending
-                       select p)
-                    .Skip(PageSize * (pageNum - 1)) // Skip() must be before Take()
-                    .Take(PageSize)
-                    .ToListAsync();
+            List<Product> products =
+                await ProductDB.GetProductAsync(_context, PageSize, pageNum);
 
             // Send list of products to view to be displayed
             return View(products);
@@ -61,9 +51,7 @@ namespace eCommerceSite.Controllers
         {
             if (ModelState.IsValid)
             {
-                // Add to DB
-                _context.Products.Add(p);
-                await _context.SaveChangesAsync();
+                await ProductDB.AddProductAsync(_context, p);
 
                 TempData["Message"] = $"{p.Title} was added successfully";
 
---
+++ eCommerceSite/Data/ProductDB.cs
@@ -0,0 +1,46 @@
+﻿using eCommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Data
+{
+    public static class ProductDB
+    {
+        /// <summary>
+        /// Returns the total count of products
+        /// </summary>
+        /// <param name="_context">Database context to use</param>
+        public static async Task<int> GetTotalProductsAsync(ProductContext _context)
+        {
+            return await (from p in _context.Products
+                        select p).CountAsync(); 
+        }
+
+        /// <summary>
+        /// Get a page worth of products
+        /// </summary>
+        /// <param name="_context">Database context to use</param>
+        /// <param name="pageSize">Number of products per page</param>
+        /// <param name="pageNum">Page of products to return</param>
+        public static async Task<List<Product>> GetProductAsync(ProductContext _context, int pageSize, int pageNum)
+        {
+            return await (from p in _context.Products
+                       orderby p.Title ascending
+                       select p)
+                    .Skip(pageSize * (pageNum - 1)) // Skip() must be before Take()
+                    .Take(pageSize)
+                    .ToListAsync();
+        }
+
+        public static async Task<Product> AddProductAsync(ProductContext _context, Product p)
+        {
+            // Add to DB
+            _context.Products.Add(p);
+            await _context.SaveChangesAsync();
+            return p;
+        }
+    }
+}
