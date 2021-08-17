a862e2eb112fcfb0837ee4ff085e5bba200b5adb
rothlisbergerc <rothlisbergercheryl@gmail.com>
2021-08-17 15:34:52 -0700

    Add registered user into DB

--- eCommerceSite/Controllers/UserController.cs
+++ eCommerceSite/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,38 @@ namespace eCommerceSite.Controllers
 {
     public class UserController : Controller
     {
+        private readonly ProductContext _context;
+
+        public UserController(ProductContext context )
+        {
+            _context = context;
+        }
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel reg)
+        {
+            if (ModelState.IsValid)
+            {
+                // Map data to user account instance
+                UserAccount acc = new UserAccount()
+                {
+                    DateOfBirth = reg.DateOfBirth,
+                    Email = reg.Email,
+                    Password = reg.Password,
+                    Username = reg.Username
+                };
+                // add to database
+                _context.UserAccounts.Add(acc);
+                await _context.SaveChangesAsync();
+                // redirect to home page
+                return RedirectToAction("Index", "home");
+            }
+
+            return View(reg);
         }
     }
 }
--- eCommerceSite/Models/UserAccount.cs
+++ eCommerceSite/Models/UserAccount.cs
@@ -33,6 +33,10 @@ namespace eCommerceSite.Models
         public string ConfirmEmail { get; set; }
 
         [Required]
+        [StringLength(20)]
+        public string Username { get; set; }
+
+        [Required]
         [DataType(DataType.Password)]
         [StringLength(120, MinimumLength = 6, ErrorMessage = "Password must be between {2} and {1}")]
         public string Password { get; set; }
